c73a909200ce45998c8c4de17a816da9d8a479af
Przemyslaw Rzeznik <przemyslaw.rzeznik@gmail.com>
2020-08-07 12:42:54 +0200

    Youtube API library added + css files generated by webpack

---
+++ config/youtube.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Laravel PHP Facade/Wrapper for the Youtube Data API v3
+|--------------------------------------------------------------------------
+|
+| Here is where you can set your key for Youtube API. In case you do not
+| have it, it can be acquired from: https://console.developers.google.com
+*/
+// https://github.com/alaouy/Youtube
+
+return [
+   'key' => env('GOOGLE_API_V3_KEY', 'YOUR_API_KEY')
+    
+];
+
+
